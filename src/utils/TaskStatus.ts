57acfe5787b5ad61d9950b4c8d1d78732b607917
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-01-22 22:59:23 +0200

    Added the Task Status enum

--- src/utils/TaskStatus.ts
+++ src/utils/TaskStatus.ts
@@ -1,1 +1,7 @@
-export const 
+export const TaskStatus = {
+    TODO: "todo",
+    DOING: "doing",
+    DONE: "done"
+} as const
+
+export type TaskStatus = (typeof TaskStatus)[keyof typeof TaskStatus];
